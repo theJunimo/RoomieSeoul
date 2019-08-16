bcbb5aca31bc04ad4c81a1fdd9e4c25cc88eebf1
HyeInJu <hyein2332@gmail.com>
2019-08-17 04:27:28 +0900

    src/components/common 공통 components 디렉터리 생성

---
+++ src/components/common/PageTemplate/PageTemplate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './PageTemplate.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const PageTemplate = () => {
+    <div className={cx('pate-template')}>
+        PageTemplate;
+    </div>
+}
